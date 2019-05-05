b12c5772278d5933bf888b84f7246b77b0e9a144
hoangnm <nguyenminhhoang210891@gmail.com>
2019-05-05 10:40:22 +0700

    use recommended eslint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import styled from 'styled-components';
 import fetch from 'isomorphic-fetch';
 
 import DonateCard from './components/DonateCard';
